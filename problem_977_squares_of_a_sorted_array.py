612fb1a9899d1268974802d9568f9e747b1b42e9
Jessica Huynh <chauhuynh.jessica@gmail.com>
2020-05-12 12:06:38 -0700

    Trying to solve using Two Pointer Methong

---
+++ problem_977_squares_of_a_sorted_array.py
@@ -0,0 +1,58 @@
+# Given an array of integers A sorted in non-decreasing order, return an array 
+# of the squares of each number, also in sorted non-decreasing order.
+
+
+
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        """
+        >>> sortedSquares([-4,-1,0,3,10])
+        [0,1,9,16,100]
+
+        >>> sortedSquares([-7,-3,2,3,11])
+        [4,9,9,49.121]
+
+        """
+        # NOTES:
+        # Take a look at the input. The array is sorted so the ends will be
+        # a higher value than the middle.
+        # So use the pointers at each end to collect the larger values.
+
+
+
+        # pseudocode:
+        # brute force:
+        # for number in A:
+        # sq_lst = []
+        # for val in A:
+            # sq_lst.append(val**2)
+        # number**2
+        # append to new list
+        # print(sq_lst)
+        # return sorted(sq_lst)
+    
+
+        # try 2
+        # for val in A:
+        # return sorted(val*val for val in A)
+
+
+        # two pointer method?:
+        # set pointers
+        left = 0 
+        right = len(A) - 1
+
+        sq_lst = []
+        # square all the values
+        sq = [val*val for val in A]
+        # sort the values in non-descending order
+        while left <= right:
+            if sq[left] > sq[right]:
+                sq_lst.append(sq[left])
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** Test Pass! \n")
